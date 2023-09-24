cb8298a28b5d4ad15a57c095fd7dcbc9330e1e03
dongjoon1251 <genos.lee@kakaocorp.com>
2023-09-24 20:46:40 +0900

    feature: 인사에 답하는 chain 추가

---
+++ ai_greeting.py
@@ -0,0 +1,28 @@
+import os
+
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+from ai_template import *
+
+llm = ChatOpenAI(temperature=0.1, max_tokens=200, model="gpt-3.5-turbo")
+
+greeting_chain = LLMChain(
+    llm=llm,
+    prompt=ChatPromptTemplate.from_template(
+        template=read_prompt_template('./template/greeting.txt')
+    ),
+    output_key="greeting",
+    verbose=True,
+)
+
+
+def greeting(user_message: str) -> str:
+    greeting = greeting_chain.run(dict(user_message=user_message))
+    print(greeting)
+    return greeting
+
+
+if __name__ == '__main__':
+    greeting = greeting('안녕?')
+    print(greeting)
